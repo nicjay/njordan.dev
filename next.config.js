f9776473728d60b40db75f7c7160ad2044a81bce
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2022-08-10 04:26:29 -0700

    Enable experimental flags

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   reactStrictMode: true,
   experimental: {
     images: {
       allowFutureImage: true,
     },
     newNextLinkBehavior: true,
+    browsersListForSwc: true,
+    legacyBrowsers: false,
   },
 };
 
